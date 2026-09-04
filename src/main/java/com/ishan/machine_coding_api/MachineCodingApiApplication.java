56c7767dc5a497785ceb00e98f79f10b24eaefda
Ishan Gupta <ishan1508@gmail.com>
2026-09-04 21:49:08 +0530

    Initialize Spring Boot machine coding server

---
+++ src/main/java/com/ishan/machine_coding_api/MachineCodingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.machine_coding_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineCodingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineCodingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishan/machine_coding_api/MachineCodingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.machine_coding_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineCodingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
